9d36dcaa87e6692750f26f08df80f465fc44a2c6
vladiere <ruthakaza@gmail.com>
2023-12-14 22:08:19 +0800

    links

--- src/pages/ServicePage.vue
+++ src/pages/ServicePage.vue
@@ -8,15 +8,23 @@
         <span class="text-h6 text-weight-regular">
           Introducing the Todo List App, powered by Vue.js. This application seamlessly blends simplicity with functionality, allowing users to organize tasks effortlessly. With a clean and intuitive user interface, managing your to-do list becomes a streamlined experience. Embrace productivity and clarity as you navigate through your daily tasks, all made possible through the efficiency of Vue.js technology.
         </span>
+        <div class="row self-start q-gutter-x-lg">
+          <q-btn flat rounded padding="none" icon="mdi-github" />
+          <q-btn flat rounded padding="none" icon="mdi-web" />
+        </div>
       </div>
     </div>
 
     <div class="row">
-      <div class="col column q-gutter-y-lg q-px-md">
-        <span class="text-h4 text-weight-bolder">Todo List APP</span>
+      <div class="col column q-gutter-y-lg q-px-md text-right">
+        <span class="text-h4 text-weight-bolder">My Unique App</span>
         <span class="text-h6 text-weight-regular">
 Experience unparalleled efficiency with my unique app, ingeniously powered by Vue.js. This innovative application redefines user interaction, offering a seamless blend of functionality and distinctive features. Dive into a world where your tasks and goals are effortlessly managed, creating a personalized and exceptional user experience, all thanks to the advanced capabilities of Vue.js.
         </span>
+        <div class="row self-end q-gutter-x-lg">
+          <q-btn flat rounded padding="none" icon="mdi-github" />
+          <q-btn flat rounded padding="none" icon="mdi-web" />
+        </div>
       </div>
       <img class="col" src="https://images.pexels.com/photos/4286932/pexels-photo-4286932.jpeg?auto=compress&cs=tinysrgb&w=600" />
     </div>
